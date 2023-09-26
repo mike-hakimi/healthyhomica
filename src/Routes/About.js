be0b247575e422a8fe6044f05199e7b6f2350c92
Mirwes Hakimi <mirwes215@gmail.com>
2023-09-26 09:00:05 -0700

    updated the about us contents

--- src/Routes/About.js
+++ src/Routes/About.js
@@ -7,10 +7,10 @@ function About(){
                <h1 className="about">About us</h1>
         <div className="about-p1">
          
-            <p>Specialties: We are an exterior window cleaning company focused on
-residential and commercial clients.
-Where we differ is in our 4 in 1 window cleaning system. Many companies will clean your windows, but we include the frames, the sills and tracks as well. Making sure your whole home shines.
-                 Please give us a chance to impress you with our on time and courteous service. </p>
+            <p>We are an East Bay born company that takes pride in servicing the area where we grew up.
+                  We excel in completing projects at a professional grade level and in a timely manner.
+                   We work hard to exceed our clients needs so we can continue the relationship for years to come.
+                     </p>
         </div>
         {/* <div className="about-p2">
             <p>Many companies will clean your windows, but we include the frames, the sills and tracks as well. Making sure your whole home shines.
